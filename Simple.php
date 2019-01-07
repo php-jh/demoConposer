762fb87bdbaa68faade3abc360d3986afd50c3a1
jiang <1169363434@qq.com>
2019-01-07 10:54:02 +0800

    chushi

---
+++ Simple.php
@@ -0,0 +1,15 @@
+<?php
+namespace testcomposer;
+/**
+ * Created by PhpStorm.
+ * User: ${蒋华}
+ * Date: 2019/1/7
+ * Time: 10:51
+ */
+
+class Simple
+{
+    public function index(){
+        return 1;
+    }
+}
